701281038360ecd28b347e24604539e416c8bf42
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2016-11-25 13:51:36 +0200

    Add new commands, clearing

--- berkshelf.el
+++ berkshelf.el
@@ -1,13 +1,13 @@
 ;;; berkshelf.el --- Interact with Berkshelf from Emacs
 
-;; Copyright (c) 2011 Tobias Svensson <tob@tobiassvensson.co.uk>
+;; Copyright (c) 2016 Alexander aka CosmonauT Vynnyk <cosmonaut.ok@zoho.com>
 
 ;; Author: Tobias Svensson <tob@tobiassvensson.co.uk>
-;; URL: http://github.com/endofunky/berkshelf.el
+;; URL: https://github.com/restaurant-ide/berkshelf.el
 ;; Keywords: berkshelf ruby
-;; Created: 31 Dec 2011
-;; Version: 1.1.1
-;; Package-Requires: ((inf-ruby "2.1") (cl-lib "0.5"))
+;; Created: 24 Nov 2016
+;; Version: 0.1.0
+;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -15,31 +15,6 @@
 
 ;; Interact with Berkshelf from Emacs.
 ;;
-;; 1) berks-open
-;;
-;;    Wraps 'berks open' which, if the given gem is installed and has been
-;;    required correctly, will open the gem's source directory with dired.
-;;
-;; 2) berks-console
-;;
-;;    Starts an inferior ruby process in the context of the current berks
-;;    using 'berks console' (requires inf-ruby to be installed).
-;;
-;; 3) berks-install, berks-update, berks-check
-;;
-;;    Runs the corresponding Berkshelf command with async-shell-command and
-;;    *Berkshelf* as the target buffer. This exists so the output won't mess
-;;    with the default buffer used by M-& and async-shell-command.
-
-;;; Install
-
-;; $ cd ~/.emacs.d/vendor
-;; $ git clone git://github.com/endofunky/berkshelf.el.git
-;;
-;; In your emacs config:
-;;
-;; (add-to-list 'load-path "~/.emacs.d/vendor/berkshelf.el")
-;; (require 'berkshelf)
 
 ;;; License:
 
@@ -61,9 +36,6 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'inf-ruby)
-;;;###autoload
-;; TODO: implement apply, contingent, cookbook <NAME>, info <NAME>, init <path>, list, package, search, shelf, show, upload, vendor, verify, viz
 
 ;;;###autoload
 (defun berks-install ()
@@ -72,79 +44,80 @@
   (berks-command "berks install"))
 
 ;;;###autoload
-(defun berks-update (&optional update-cmd-args)
-  "Run berks update for the current berks."
-  (interactive "P")
-  (let ((command "berks update"))
-    ;; For customization of the command with prefix arg.
-    (setq command (if update-cmd-args
-                      (read-string "Run: " (concat command " "))
-                    command))
-
-    (berks-command command)))
-
-;;;###autoload
-(defun berks-berksfile (&optional berksfile)
-  "Set BERKS_BERKSFILE environment variable."
-  (interactive
-   (list
-    (let ((default-p
-            (let ((berksfile-dir (berks-locate-berksfile)))
-              (if (not berksfile-dir)
-                  "Berksfile"
-                (concat berksfile-dir "Berksfile")))))
-    (read-string (format "Berksfile (%s): " default-p)
-                 default-p nil default-p))))
-  (if berksfile
-      (if (file-readable-p berksfile)
-          (progn
-            (setq berks-gem-list-cache (make-hash-table))
-            (setenv "BERKS_BERKSFILE" berksfile)
-            (message "BERKS_BERKSFILE set to: %s." berksfile))
-        (message "Warning: couldn't read file \"%s\". BERKS_BERKSFILE unchanged." berksfile))
-    (setenv "BERKS_BERKSFILE")))
+(defun berks-list ()
+  "Run berks install for the current berks."
+  (interactive)
+  (berks-command "berks list"))
 
 ;;;###autoload
 (defun berks-outdated ()
-  "List installed gems with newer versions available."
+  "Run berks install for the current berks."
   (interactive)
   (berks-command "berks outdated"))
 
 ;;;###autoload
-(defun berks-show ()
-  "Shows all gems that are part of the berks, or the path to a given gem."
-  (interactive)
-  (berks-command "berks show"))
+(defun berks-search (cookbook)
+  "Run berks install for the current berks."
+  (interactive "sCookbook Name: ")
+  (berks-command (concat "berks search " cookbookk)))
 
 ;;;###autoload
-(defun berks-version ()
-  "Prints version information."
+(defun berks-info (cookbook)
+  "Run berks install for the current berks."
+  (interactive "sCookbook Name: ")
+  (berks-command (concat "berks info " cookbookk)))
+
+;;;###autoload
+(defun berks-show (cookbook)
+  "Run berks install for the current berks."
+  (interactive "sCookbook Name: ")
+  (berks-command (concat "berks show " cookbookk)))
+
+;;;###autoload
+(defun berks-update (cookbook)
+  "Run berks install for the current berks."
+  (interactive "sCookbook Name: ")
+  (berks-command (concat "berks update " cookbookk)))
+
+;;;###autoload
+(defun berks-upload (cookbook)
+  "Run berks install for the current berks."
+  (interactive "sCookbook Name: ")
+  (berks-command (concat "berks upload " cookbookk)))
+
+;;;###autoload
+(defun berks-vendor (path)
+  "Run berks install for the current berks."
+  (interactive "sPath Name: ")
+  (berks-command (concat "berks vendor " path)))
+
+;;;###autoload
+(defun berks-package (path)
+  "Run berks install for the current berks."
+  (interactive "sDirectory Name: ")
+  (berks-command (concat "berks package " path)))
+
+;;;###autoload
+(defun berks-install ()
+  "Run berks install for the current berks."
   (interactive)
-  (shell-command "berks version"))
+  (berks-command "berks install"))
+
+;;;###autoload
+(defun berks-verify ()
+  "Run berks install for the current berks."
+  (interactive)
+  (berks-command "berks verify"))
+
+;;;###autoload
+(defun berks-viz ()
+  "Run berks install for the current berks."
+  (interactive)
+  (berks-command "berks viz"))
 
 (defun berks-command (cmd)
   "Run cmd in an async buffer."
   (async-shell-command cmd "*Berkshelf*"))
-
-(defun berks-gem-location (gem-name)
-  "Returns the location of the given gem, or 'no-berksfile if the
-Berksfile could not be found, or nil if the Gem could not be
-found."
-  (let ((berkshelf-stdout
-         (shell-command-to-string
-          (format "berks show %s" (shell-quote-argument gem-name))))
-        (remote (file-remote-p default-directory)))
-    (cond
-     ((string-match "Could not locate Berksfile" berkshelf-stdout)
-      'no-berksfile)
-     ((string-match "Could not find " berkshelf-stdout)
-      nil)
-     (t
-      (concat remote
-              (replace-regexp-in-string
-               "Resolving dependencies...\\|\n" ""
-               berkshelf-stdout)
-              "/")))))
 
 (defvar berks-gem-list-cache
   (make-hash-table)
@@ -161,44 +134,6 @@ found."
                      default-directory))
              nil)
             ((berks-locate-berksfile (expand-file-name ".." dir))))))
-
-(defun berks-list-gems-cached ()
-  (let* ((berksfile-dir (berks-locate-berksfile))
-         (gem-list (gethash berksfile-dir berks-gem-list-cache)))
-    (if (not berksfile-dir)
-        nil
-      (unless gem-list
-        (print (format "Don't have directory %s in cache yet, updating." berksfile-dir))
-        (setq gem-list (berks-list-gems))
-        (puthash berksfile-dir gem-list berks-gem-list-cache))
-      gem-list)))
-
-(defun berks-list-gems ()
-  (save-excursion
-    (let* ((cmd "berks list")
-           (berks-out (shell-command-to-string cmd))
-           (berks-lines (split-string berks-out "\n")))
-
-      (defun parse-berks-list-line (line)
-        (cond
-         ((string-match "^  \\* \\([^\s]+\\).*$" line)
-          (match-string 1 line))
-         ((string-match "Could not \\(find\\|locate\\)" line)
-          (message line) nil)
-         ((string-match "Gems included by the berks:\\|^ *$" line)
-          nil)
-         (t
-          (message "Warning: couldn't parse line from \"%s\":\n%s"
-                   cmd line)
-          nil)))
-
-      (remq nil (mapcar 'parse-berks-list-line berks-lines)))))
-
-(defun berks-list-gem-paths ()
-  (save-excursion
-    (let* ((cmd "berks list --paths")
-           (berks-out (shell-command-to-string cmd)))
-      (split-string berks-out "\n"))))
 
 (provide 'berkshelf)
 ;;; berkshelf.el ends here.
